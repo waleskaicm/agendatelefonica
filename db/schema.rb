5cb5517320f2f4f4ac700b236d772ae469224eec
Waleska Miranda <waleska@srvdesk.com.br>
2017-02-02 22:38:08 -0200

    Ajustes css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201003224) do
+ActiveRecord::Schema.define(version: 20170131213935) do
 
   create_table "agendas", force: :cascade do |t|
     t.string   "nome"
@@ -22,11 +22,6 @@ ActiveRecord::Schema.define(version: 20170201003224) do
     t.string   "cidade"
     t.string   "estado"
     t.string   "cep"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "searches", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
